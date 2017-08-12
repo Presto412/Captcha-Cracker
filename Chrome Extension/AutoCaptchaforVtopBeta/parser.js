aa303786150e9035d4480ba59ee108cae4e85bbc
Priyansh Jain <priyanshjain412@gmail.com>
2017-08-12 23:31:50 +0530

    added googleanalytics

--- Chrome Extension/AutoCaptchaforVtopBeta/parser.js
+++ Chrome Extension/AutoCaptchaforVtopBeta/parser.js
@@ -1,4 +1,4 @@
-function CaptchaParse(imgarr) {
+﻿function CaptchaParse(imgarr) {
   var bitmaps={
 "1":[
 	[255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255],
@@ -1243,6 +1243,14 @@ function SolveCap($img){
   k[0].innerHTML="<center>AutoCaptcha-Made with ♥,CollegeCODE</center>";
 }
 var img = document.getElementsByTagName("img")[1];
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-104495999-1', 'auto');
+ga('send', 'pageview');
+
 
 /*var changebutton=document.getElementsByTagName('button')[0];
 changebutton.parentNode.removeChild(changebutton);*/
