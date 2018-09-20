a8d3f6d8d052a31a7a8de889db3796eb596e10f7
Priyansh Jain <priyansh.jain0246@gmail.com>
2018-09-21 01:26:13 +0530

    Added cropped characters

--- Python/preprocessing/crop_chars.py
+++ Python/preprocessing/crop_chars.py
@@ -1,14 +1,13 @@
 from PIL import Image
 import os
 ct = 0
-if not os.path.exists("Characters"):
-    os.mkdir("Characters")
-for i in range(0, 100):
-    fn = "captchas//"+str(i)+".png"
-    img = Image.open(fn)
+if not os.path.exists("cropped-chars"):
+    os.mkdir("cropped-chars")
+for image in os.listdir("noise-reduced-images"):
+    image_path = os.path.join("noise-reduced-images", image)
+    img = Image.open(image_path)
     for j in range(30, 181, 30):
         ct += 1
-        fin = "Characters\\"+str(ct)+".png"
+        character_path = os.path.join("cropped-chars", str(ct) + ".png")
         ch = img.crop((j-30, 12, j, 44))
-        ch.save(fin)
-    img.save(fn)
+        ch.save(character_path)
