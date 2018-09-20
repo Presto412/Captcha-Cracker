f74dd1336ef0e2e78f9b85287246fdda92fa69d1
Priyansh Jain <priyansh.jain0246@gmail.com>
2018-09-21 00:43:01 +0530

    formatting

--- Python/preprocessing/scripts/Bestchar.py
+++ Python/preprocessing/scripts/Bestchar.py
@@ -1,19 +1,19 @@
 from PIL import Image
 import os
-order="123456789abcdefghijklmnpqrstuvwxyz"
-for i in range(0,len(order)):
-    small=750
-    char=order[i]
-    fn=os.listdir("SortedCharacters\\"+char)
+order = "123456789abcdefghijklmnpqrstuvwxyz"
+for i in range(0, len(order)):
+    small = 750
+    char = order[i]
+    fn = os.listdir("SortedCharacters\\"+char)
     for i in fn:
-        ct=0
-        im=Image.open("SortedCharacters\\"+char+"\\"+i)
-        pix=im.load()
-        for y in range(0,32):
-            for x in range(0,30):
-                if pix[x,y]==(0,0,0):
-                    ct+=1
-        if ct<=small:
-            small=ct
+        ct = 0
+        im = Image.open("SortedCharacters\\"+char+"\\"+i)
+        pix = im.load()
+        for y in range(0, 32):
+            for x in range(0, 30):
+                if pix[x, y] == (0, 0, 0):
+                    ct += 1
+        if ct <= small:
+            small = ct
             im.save("Chars\\"+char+".png")
 print "it workd"
--- Python/preprocessing/scripts/getchars.py
+++ Python/preprocessing/scripts/getchars.py
@@ -1,14 +1,14 @@
 from PIL import Image
 import os
-ct=0
+ct = 0
 if not os.path.exists("Characters"):
     os.mkdir("Characters")
-for i in range(0,100):
-    fn="captchas//"+str(i)+".png"
-    img=Image.open(fn)
-    for j in range(30,181,30):
-        ct+=1
-        fin="Characters\\"+str(ct)+".png"
-        ch=img.crop((j-30,12,j,44))
+for i in range(0, 100):
+    fn = "captchas//"+str(i)+".png"
+    img = Image.open(fn)
+    for j in range(30, 181, 30):
+        ct += 1
+        fin = "Characters\\"+str(ct)+".png"
+        ch = img.crop((j-30, 12, j, 44))
         ch.save(fin)
     img.save(fn)
