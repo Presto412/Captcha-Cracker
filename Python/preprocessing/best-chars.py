c7bc875727d08aeaf01a0d119e0a52da3f58c1e9
Priyansh Jain <priyansh.jain0246@gmail.com>
2018-09-21 02:53:26 +0530

    Added best among sorted

---
+++ Python/preprocessing/best-chars.py
@@ -0,0 +1,19 @@
+import os
+from PIL import Image
+
+folder_name = "best-among-sorted"
+input_folder_name = "sorted-characters"
+if not os.path.exists(folder_name):
+    os.mkdir(folder_name)
+
+for _, main_dir, _ in os.walk(input_folder_name):
+    for sub_dir in main_dir:
+        min = 10000
+        for image in os.listdir(os.path.join(input_folder_name, sub_dir)):
+            img = Image.open(os.path.join(input_folder_name, sub_dir, image))
+            black_count = img.getcolors()[1][0]
+            print("Black count for folder " + sub_dir + " is ", black_count)
+            if black_count < min:
+                min = black_count
+                print("New min for folder " + sub_dir + " is " + str(min))
+                img.save(os.path.join(folder_name, sub_dir + '.png'))
