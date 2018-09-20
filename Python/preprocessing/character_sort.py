5d7fd909f06f02220f5ec129510b52da7e00e0c2
Priyansh Jain <priyansh.jain0246@gmail.com>
2018-09-21 02:35:46 +0530

    sorted characters

--- Python/preprocessing/character_sort.py
+++ Python/preprocessing/character_sort.py
@@ -1,28 +1,61 @@
 from PIL import Image
 import os
-if not os.path.exists("SortedCharacters"):
-    os.mkdir("SortedCharacters")
-order = "123456789abcdefghijklmnpqrstuvwxyz"
-for i in range(0, len(order)):
-    black = 0
-    im1 = Image.open("Chars\\"+order[i]+".png")
-    if not os.path.exists("SortedCharacters\\"+order[i]):
-        os.mkdir("SortedCharacters\\"+order[i])
-    im1.save("SortedCharacters\\"+order[i]+"\\First.png")
-    pix1 = im1.load()
-    for y in range(0, 32):
-        for x in range(0, 30):
-            if pix1[x, y] == (0, 0, 0):
-                black += 1
-    for j in range(1, 601):
-        match = 0
-        im2 = Image.open("Characters\\"+str(j)+".png")
-        pix2 = im2.load()
-        for y in range(0, 32):
-            for x in range(0, 30):
-                if pix1[x, y] == pix2[x, y] and pix2[x, y] == (0, 0, 0):
-                    match += 1
-        if float(match)/float(black) >= 0.95:
-            fp = "SortedCharacters\\"+order[i] + \
-                "\\"+order[i]+"-"+str(4000+j)+".png"
-            im2.save(fp)
+import shutil
+import json
+
+
+if not os.path.exists("sorted-characters"):
+    os.mkdir("sorted-characters")
+else:
+    shutil.rmtree('sorted-characters')
+    os.mkdir("sorted-characters")
+
+
+def get_similarity(img_1, img_2):
+    pix1 = img_1.convert('L').load()
+    pix2 = img_2.convert('L').load()
+    match_count = 0
+    pix1_black_count = 0
+    pix2_black_count = 0
+
+    for y in range(0, img_1.height):
+        for x in range(0, img_1.width):
+            if pix1[x, y] == pix2[x, y] and pix2[x, y] == 0:
+                match_count += 1
+            if pix2[x, y] == 0:
+                pix2_black_count += 1
+            if pix1[x, y] == 0:
+                pix1_black_count += 1
+
+    # no of pixels have more than 10% variation
+    if float(pix1_black_count - pix2_black_count) / float(pix1_black_count + pix2_black_count) >= 1.5:
+        return 0
+    return float(match_count)/float(pix2_black_count)
+
+
+visited_images = []
+similarity_dict = {}
+count = 0
+for image_1_path in os.listdir("cropped-chars"):
+    img_1 = Image.open(os.path.join("cropped-chars", image_1_path))
+    visited_images.append(image_1_path)
+    for image_2_path in os.listdir("cropped-chars"):
+        if image_2_path in visited_images:
+            continue
+        img_2 = Image.open(os.path.join("cropped-chars", image_2_path))
+        if get_similarity(img_1, img_2) >= 0.95:
+            visited_images.append(image_2_path)
+            if image_1_path not in similarity_dict.keys():
+                count += 1
+                similarity_dict[image_1_path] = [image_2_path]
+                os.mkdir(os.path.join("sorted-characters", str(count)))
+                img_1.save(os.path.join(
+                    "sorted-characters", str(count), image_1_path))
+                img_2.save(os.path.join(
+                    "sorted-characters", str(count), image_2_path))
+                continue
+            similarity_dict[image_1_path].append(image_2_path)
+            img_1.save(os.path.join(
+                "sorted-characters", str(count), image_1_path))
+            img_2.save(os.path.join(
+                "sorted-characters", str(count), image_2_path))
